215b2df1a99c349c5b0a286721ef0348a59455ae
Zulbukharov <l1pt0n1905@gmail.com>
2023-01-28 00:06:07 +0600

    solve: with-time-error

--- tasks/03-go-errors-concept/with-time-error/with_time_error.go
+++ tasks/03-go-errors-concept/with-time-error/with_time_error.go
@@ -1,17 +1,39 @@
 package errs
 
 import (
+	"errors"
+	"fmt"
 	"time"
 )
 
-type WithTimeError struct { // Реализуй меня.
+type WithTimeError struct {
+	creationDate time.Time
+	err          error
 }
 
 func NewWithTimeError(err error) error {
 	return newWithTimeError(err, time.Now)
 }
 
+func (e *WithTimeError) Error() string {
+	return fmt.Sprintf("%v, occurred at: %s", e.err, e.creationDate)
+}
+
+func (e *WithTimeError) Is(target error) bool {
+	return errors.Is(e.err, target)
+}
+
+func (e *WithTimeError) Time() time.Time {
+	return e.creationDate
+}
+
+func (e *WithTimeError) As(target any) bool {
+	return errors.As(e.err, target)
+}
+
 func newWithTimeError(err error, timeFunc func() time.Time) error {
-	// Реализуй меня.
-	return nil
+	return &WithTimeError{
+		creationDate: timeFunc(),
+		err:          err,
+	}
 }
