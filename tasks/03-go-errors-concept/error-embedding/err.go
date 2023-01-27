631119c9548e9ee9b305def1ae6581e086069452
Zulbukharov <l1pt0n1905@gmail.com>
2023-01-27 14:41:59 +0600

    solve:  error-embedding

--- tasks/03-go-errors-concept/error-embedding/err.go
+++ tasks/03-go-errors-concept/error-embedding/err.go
@@ -9,3 +9,51 @@ var (
 )
 
 // Реализуй тип Err и типы для ошибок выше, используя его.
+
+type AlreadyDoneError struct {
+	Err
+}
+
+func (e *AlreadyDoneError) Error() string {
+	return e.Err.Error()
+}
+
+type InconsistentDataError struct {
+	Err
+}
+
+func (e *InconsistentDataError) Error() string {
+	return e.Err.Error()
+}
+
+type InvalidIDError struct {
+	Err
+}
+
+func (e *InvalidIDError) Error() string {
+	return e.Err.Error()
+}
+
+type NotReadyError struct {
+	Err
+}
+
+func (e *NotReadyError) Error() string {
+	return e.Err.Error()
+}
+
+type NotFoundError struct {
+	Err
+}
+
+func (e *NotFoundError) Error() string {
+	return e.Err.Error()
+}
+
+type Err struct {
+	msg string
+}
+
+func (e *Err) Error() string {
+	return e.msg
+}
