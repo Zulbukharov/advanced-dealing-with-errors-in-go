0cda99f36ec0436466b9a1adf620e62fc5527c75
Zulbukharov <l1pt0n1905@gmail.com>
2023-01-27 14:45:50 +0600

    solve: errors-factory

--- tasks/03-go-errors-concept/errors-factory/errors.go
+++ tasks/03-go-errors-concept/errors-factory/errors.go
@@ -3,9 +3,16 @@ package errors
 // NewError возвращает новое значение-ошибку, текст которой является msg.
 // Две ошибки с одинаковым текстом, созданные через NewError, не равны между собой:
 //
-//  NewError("end of file") != NewError("end of file")
-//
+//	NewError("end of file") != NewError("end of file")
 func NewError(msg string) error {
 	// Реализуй меня.
-	return nil
+	return &errorStruct{msg}
+}
+
+type errorStruct struct {
+	s string
+}
+
+func (e *errorStruct) Error() string {
+	return e.s
 }
