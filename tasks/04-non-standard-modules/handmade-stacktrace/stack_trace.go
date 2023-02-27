791403fedec2582061fd0c319e1efa2bed22ce34
Zulbukharov <l1pt0n1905@gmail.com>
2023-02-27 15:20:23 +0600

    feat: handmade-stacktrace

--- tasks/04-non-standard-modules/handmade-stacktrace/stack_trace.go
+++ tasks/04-non-standard-modules/handmade-stacktrace/stack_trace.go
@@ -1,5 +1,12 @@
 package stacktrace
 
+import (
+	"fmt"
+	"log"
+	"runtime"
+	"strings"
+)
+
 const maxStacktraceDepth = 32
 
 type Frame uintptr
@@ -9,20 +16,52 @@ func (f Frame) pc() uintptr {
 }
 
 func (f Frame) String() string {
-	// Реализуй меня.
-	return ""
+	fun := runtime.FuncForPC(f.pc())
+	if fun == nil {
+		return ""
+	}
+
+	funcName := fun.Name()
+	if v := strings.LastIndex(funcName, "/"); v != -1 {
+		funcName = funcName[v+1:]
+	}
+
+	fileStr, line := fun.FileLine(f.pc())
+
+	if n := strings.LastIndex(fileStr, "/"); n != -1 {
+		if m := strings.LastIndex(fileStr[:n], "/"); m != -1 {
+			fileStr = fileStr[m+1:]
+		} else {
+			fileStr = fileStr[n:]
+		}
+	}
+
+	return fmt.Sprintf("%s\n%s:%d", funcName, fileStr, line)
 }
 
 type StackTrace []Frame
 
 func (s StackTrace) String() string {
+	b := strings.Builder{}
+	for _, l := range s {
+		b.WriteString(l.String())
+		b.WriteByte('\n')
+	}
+	log.Println(b.String())
 	// Реализуй меня.
-	return ""
+	return b.String()
 }
 
 // Trace возвращает стектрейс глубиной не более maxStacktraceDepth.
 // Возвращаемый стектрейс начинается с того места, где была вызвана Trace.
 func Trace() StackTrace {
-	// Реализуй меня.
-	return nil
+	pc := make([]uintptr, maxStacktraceDepth)
+	n := runtime.Callers(2, pc)
+	st := make(StackTrace, 0, n)
+	log.Println(n, len(st))
+
+	for _, c := range pc[:n] {
+		st = append(st, Frame(c))
+	}
+	return st
 }
